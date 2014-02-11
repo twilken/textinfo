81c5ac8c19b68b0fd0c036ffe9aca8ca28c8b5b0
tenpeoplemeet <tenpeoplemeet@gmail.com>
2014-02-11 21:17:24 +0100

    Sort words by word count

--- textinfo.go
+++ textinfo.go
@@ -5,10 +5,36 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"sort"
 	"strings"
 )
 
 var flagPath = flag.String("path", "./text.txt", "The textfile you want to analyse.")
+
+// A data structure to hold a key/value pair.
+type Pair struct {
+	Key   string
+	Value int
+}
+
+// A slice of Pairs that implements sort.Interface to sort by Value.
+type PairList []Pair
+
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value }
+
+// A function to turn a map into a PairList, then sort and return it.
+func sortMapByValue(m map[string]int) PairList {
+	p := make(PairList, len(m))
+	i := 0
+	for k, v := range m {
+		p[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(p)
+	return p
+}
 
 func init() {
 	flag.Parse()
@@ -52,9 +78,9 @@ func main() {
 	text := readText(flagPath)
 	words := extractWords(text)
 	counts := countWords(words)
+	sorted := sortMapByValue(*counts)
 
-	fmt.Println("Info about:", *flagPath)
-	for key, val := range *counts {
-		fmt.Printf("%5v %v\n", val, key)
+	for _, pair := range sorted {
+		fmt.Printf("%5v %v\n", pair.Value, pair.Key)
 	}
 }
