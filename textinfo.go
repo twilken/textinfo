9b2c1da0d6d907cb78039f88174e31372581bebd
tenpeoplemeet <tenpeoplemeet@gmail.com>
2014-02-12 06:31:10 -0800

    Only print the 45 most frequent words. Close #1

--- textinfo.go
+++ textinfo.go
@@ -1,95 +1,96 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"sort"
-	"strings"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "sort"
+    "strings"
 )
 
 var flagPath = flag.String("path", "./text.txt", "The textfile you want to analyse.")
 
 // A data structure to hold a key/value pair.
 type Pair struct {
-	Key   string
-	Value int
+    Key   string
+    Value int
 }
 
 // A slice of Pairs that implements sort.Interface to sort by Value.
 type PairList []Pair
 
 func (p PairList) Swap(i, j int) { 
-	p[i], p[j] = p[j], p[i] 
+    p[i], p[j] = p[j], p[i] 
 }
 
 func (p PairList) Len() int { 
-	return len(p) 
+    return len(p) 
 }
 
 func (p PairList) Less(i, j int) bool { 
-	return p[i].Value > p[j].Value 
+    return p[i].Value > p[j].Value 
 }
 
 // A function to turn a map into a PairList, then sort and return it.
 func sortMapByValue(m map[string]int) PairList {
-	p := make(PairList, len(m))
-	i := 0
-	for k, v := range m {
-		p[i] = Pair{k, v}
-		i++
-	}
-	sort.Sort(p)
-	return p
+    p := make(PairList, len(m))
+    i := 0
+    for k, v := range m {
+        p[i] = Pair{k, v}
+        i++
+    }
+    sort.Sort(p)
+    return p
 }
 
 func init() {
-	flag.Parse()
+    flag.Parse()
 }
 
 func readText(path *string) *string {
-	dat, err := ioutil.ReadFile(*flagPath)
-	if err != nil {
-		log.Fatal("Could not read file at " + *path)
-	}
-	text := string(dat)
-	return &text
+    dat, err := ioutil.ReadFile(*flagPath)
+    if err != nil {
+        log.Fatal("Could not read file at " + *path)
+    }
+    text := string(dat)
+    return &text
 }
 
 func extractWords(text *string) *[]string {
-	lowercase := strings.ToLower(*text)
-	words := strings.FieldsFunc(lowercase, func(r rune) bool {
-		switch r {
-		case '.', ',', '!', '?', ' ', '"', ':', ';', '(', ')', '\n', '\r',
-			'\t', '\v', '\\', '/', '\f', '\a', '\b':
-			return true
-		}
-		return false
-	})
-	return &words
+    lowercase := strings.ToLower(*text)
+    words := strings.FieldsFunc(lowercase, func(r rune) bool {
+        switch r {
+        case '.', ',', '!', '?', ' ', '"', ':', ';', '(', ')', '\n', '\r',
+            '\t', '\v', '\\', '/', '\f', '\a', '\b':
+            return true
+        }
+        return false
+    })
+    return &words
 }
 
 func countWords(words *[]string) *map[string]int {
-	counts := make(map[string]int)
-	for _, word := range *words {
-		_, present := counts[word]
-		if present {
-			counts[word]++
-		} else {
-			counts[word] = 1
-		}
-	}
-	return &counts
+    counts := make(map[string]int)
+    for _, word := range *words {
+        _, present := counts[word]
+        if present {
+            counts[word]++
+        } else {
+            counts[word] = 1
+        }
+    }
+    return &counts
 }
 
 func main() {
-	text := readText(flagPath)
-	words := extractWords(text)
-	counts := countWords(words)
-	sorted := sortMapByValue(*counts)
+    text := readText(flagPath)
+    words := extractWords(text)
+    counts := countWords(words)
+    sorted := sortMapByValue(*counts)
 
-	for _, pair := range sorted {
-		fmt.Printf("%5v %v\n", pair.Value, pair.Key)
-	}
+    numberOfWordsToPrint := 45
+    for i := 0; i < numberOfWordsToPrint; i++ {
+        fmt.Printf("%5v %v\n", sorted[i].Value, sorted[i].Key)
+    }
 }
